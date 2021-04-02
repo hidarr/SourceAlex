1061e17489742272900b5c305d41102e812d3861
SourceAlex <81820905+SourceAlex@users.noreply.github.com>
2021-04-03 00:16:33 +0300

    Update Alex.lua

--- Alex.lua
+++ Alex.lua
@@ -8796,7 +8796,7 @@ keyboard.inline_keyboard = {
 {text =numjoine, callback_data=msg.sender_user_id_.."/UnKed@"..numjoine..":"..numjoine},{text = num3, callback_data=msg.sender_user_id_.."/unlockjoine"},
 },
 }
-end
+
 
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Texti).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
@@ -9100,4 +9100,5 @@ if data and data.type_ and data.type_.channel_ and data.type_.channel_.status_ a
 database:sadd(bot_id..'Alex:Chek:Groups',v)  
 end end,nil)
 end;end;end
+end
 end
