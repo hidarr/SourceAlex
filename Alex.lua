9908b418a072e45dbd04e2cfbe5d4464689fb334
SourceAlex <81820905+SourceAlex@users.noreply.github.com>
2021-04-03 00:03:37 +0300

    Update Alex.lua

--- Alex.lua
+++ Alex.lua
@@ -8756,6 +8756,8 @@ https.request("https://api.telegram.org/bot" .. token .. "/restrictChatMember?ch
 end
 end
 end
+end
+end
 if data.ID == "UpdateChannel" then 
 if data.channel_.status_.ID == "ChatMemberStatusKicked" then 
 database:srem(bot_id..'Alex:Chek:Groups','-100'..data.channel_.id_)  
@@ -8796,7 +8798,7 @@ keyboard.inline_keyboard = {
 },
 }
 end
-end
+
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Texti).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 https.request("https://api.telegram.org/bot"..token.."/restrictChatMember?chat_id="..msg.chat_id_.."&user_id="..msg.sender_user_id_)
@@ -9099,4 +9101,3 @@ if data and data.type_ and data.type_.channel_ and data.type_.channel_.status_ a
 database:sadd(bot_id..'Alex:Chek:Groups',v)  
 end end,nil)
 end;end;end
-end
